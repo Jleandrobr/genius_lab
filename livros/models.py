6769db5d47340e857f320c982db3f5cf8726e08a
Jleandrobr <leandro.fernandes@academico.ifpb.edu.br>
2024-10-29 13:05:45 -0300

    implementando model de livros

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Livro(models.Model):
+    GENEROS = [
+        ('Ficcao', 'Ficção'),
+        ('NaoFiccao', 'Não-Ficção'),
+        ('Romance', 'Romance'),
+        ('Ciencia', 'Ciência'),
+        ('Biografia', 'Biografia'),
+        ('Fantasia', 'Fantasia'),
+        ('Terror', 'Terror'),
+    ]
+
+    titulo = models.CharField(max_length=255, verbose_name="Título do Livro")
+    autor = models.CharField(max_length=255, verbose_name="Autor")
+    isbn = models.CharField(max_length=13, unique=True, verbose_name="ISBN")
+    editora = models.CharField(max_length=255, verbose_name="Editora")
+    ano_publicacao = models.PositiveIntegerField(verbose_name="Ano de Publicação")
+    genero = models.CharField(max_length=50, choices=GENEROS, verbose_name="Gênero")
+    quantidade_total = models.PositiveIntegerField(verbose_name="Quantidade Total")
+    quantidade_disponivel = models.PositiveIntegerField(verbose_name="Quantidade Disponível", default=0)
+    descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
+
+    def __str__(self):
+        return self.titulo
+
+    def salvar(self, *args, **kwargs):
+        if not self.isbn:
+            self.isbn = str(uuid.uuid4().int)[:13]  # Gera um ISBN único de 13 dígitos
+        # Garanta que a quantidade disponível não exceda a quantidade total.
+        if self.quantidade_disponivel > self.quantidade_total:
+            self.quantidade_disponivel = self.quantidade_total
+        super().save(*args, **kwargs)
