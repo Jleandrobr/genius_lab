d40c560722120883b97a913a495a69515a1a1c0f
José Leandro Brasileiro <88900122+Jleandrobr@users.noreply.github.com>
2024-11-06 23:04:12 -0300

    fix: model livro

--- livros/models.py
+++ livros/models.py
@@ -29,7 +29,6 @@ class Livro(models.Model):
         return self.titulo
 
     def save(self, *args, **kwargs):
-    def save(self, *args, **kwargs):
         if not self.isbn:
             self.isbn = str(uuid.uuid4().int)[:13] # Gera um ISBN aleatório de 13 dígitos
 
